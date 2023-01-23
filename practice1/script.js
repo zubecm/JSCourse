7d7eae1bc0ae0b79dbf80a5d89ba497e3ed75dbb
Ivan <Mi@gmail.com>
2023-01-23 12:20:20 +0100

    23.01

--- practice1/script.js
+++ practice1/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,9 +7,22 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-      ratingLastFilm = prompt('На сколько оцените его?', '');
+for (i = 0; i < 2; i++) { 
+    lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+    ratingLastFilm = prompt('На сколько оцените его?', '');
+    if (lastFilm != null && ratingLastFilm != null && lastFilm != '' && ratingLastFilm != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = ratingLastFilm;
+    } else {
+        i--;
+    }
+}
 
-personalMovieDB.movies[lastFilm] = ratingLastFilm;
-
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+    console.log('Вы не любите фильмы')
+} else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('Вы хороший зритель')
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Error')
+}
